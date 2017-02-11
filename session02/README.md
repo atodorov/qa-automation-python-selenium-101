111eeede41dd19dfbae6236f7ca38a87d0cc47e1
Alexander Todorov <atodorov@mrsenko.com>
2017-02-11 17:22:10 +0200

    Add easier tasks for session02

---
+++ session02/README.md
@@ -0,0 +1,55 @@
+# Session 02: Data types and data structures
+
+## Preparation
+
+* Read chapter 03 from Dive into Python;
+* Read chapter [Built-in types](https://docs.python.org/3/library/stdtypes.html)
+from Python's documentation.
+
+## Main data types
+
+In Python these are `int`, `float`, `boolean`, `str`, `list`, `dict`, `tuple` and `set`!
+Operators for these types are defined in the documentation and will be used in the
+tasks below. We will explain them as we go along!
+
+
+
+## Tasks & homework
+
+* Implement a function `num_add(a, b)` which adds two numbers together
+* Implement a function `num_sub(a, b)` which subtracts two numbers
+* Implement a function `num_mul(a, b)` which multiplies two numbers
+* Implement a function `num_div(a, b)` which divides the two numbers
+* Implement a function `num_floor(a, b)` which implements floor division
+* Implement a function `num_rem(a, b)` which implements remainder division
+* Define boolean constant `IS_TRUE`
+* Define boolean constant `IS_FALSE`
+* Define the `PANCAKE_INGREDIENTS` dictionary to include the following keys and values
+
+        flour - 2
+        eggs - 4
+        milk - 200
+        butter - False
+        salt - 0.001
+
+* Implement a function `ingredient_exists(ingr, dict)` which returns boolean if the
+ingredient `ingr` exists in the dictionary `dict
+* Implement a function `fatten_pancakes(dict)` which returns a dictionary. The return
+value contains the pancake ingredients where `eggs == 6` and `butter == True`.
+**NOTE:** don't change the `PANCAKE_INGREDIENTS` constant! Use `dict.copy()` method!
+* Implement a function `add_sugar(dict)` which adds 'sugar' to the list of ingredients
+and returns a new dictionary
+* Implement a function `remove_salt(dict)` which removes 'salt' from the list of
+igredients and returns a new dictionary
+* Define a list called `FIBONACCI_NUMBERS` which contains the first 12 Fibonacci numbers:
+
+        1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+* Implement a function `add_fibonacci(lst)` which extends the list of numbers with
+the next one
+* Implement a function `fib_exists(lst, n)` which returns boolean. The function checks
+if the number `n` exists in the Fibonacci sequence `lst`
+* Implement a function `which_fib(lst, n)` which returns integer. This is the index
+of the number `n` inside the sequence `lst` counting from 1.
+
+**TIP:** Use the `test.py` file to validate your solution is correct.
---
+++ session02/test.py
@@ -0,0 +1,126 @@
+import unittest
+from solution import *
+
+class TestSolution(unittest.TestCase):
+    def test_num_add(self):
+        self.assertEqual(num_add(1, 2), 3)
+        self.assertEqual(num_add(2, 1), 3)
+        self.assertEqual(num_add(1.5, 2.5), 4.0)
+
+    def test_num_sub(self):
+        self.assertEqual(num_sub(1, 2), -1)
+        self.assertEqual(num_sub(2, 1), 1)
+        self.assertEqual(num_sub(1.5, 2.5), -1.0)
+
+    def test_num_mul(self):
+        self.assertEqual(num_mul(3, 2), 6)
+        self.assertEqual(num_mul(2, 3), 6)
+        self.assertEqual(num_mul(2.5, 4), 10.0)
+
+    def test_num_div(self):
+        self.assertEqual(num_div(3, 2), 1)
+        self.assertEqual(num_div(2, 3), 0)
+        self.assertEqual(num_div(3.0, 2), 1.5)
+
+    def test_num_floor(self):
+        self.assertEqual(num_floor(10, 4), 2)
+        self.assertEqual(num_floor(4, 10), 0)
+
+    def test_num_rem(self):
+        self.assertEqual(num_rem(10, 4), 2)
+        self.assertEqual(num_rem(10, 5), 0)
+        self.assertEqual(num_rem(4, 10), 4)
+
+    def test_boolean_constants(self):
+        self.assertTrue(IS_TRUE)
+        self.assertFalse(IS_FALSE)
+
+    def test_pancake_ingredients(self):
+        for key in PANCAKE_INGREDIENTS.keys():
+            self.assertTrue(key in ['flour', 'eggs', 'milk', 'butter', 'salt'])
+        self.assertEqual(PANCAKE_INGREDIENTS['flour'], 2)
+        self.assertEqual(PANCAKE_INGREDIENTS['eggs'], 4)
+        self.assertEqual(PANCAKE_INGREDIENTS['milk'], 200)
+        self.assertFalse(PANCAKE_INGREDIENTS['butter'])
+        self.assertEqual(PANCAKE_INGREDIENTS['salt'], 0.001)
+
+    def test_ingredient_exists(self):
+        self.assertTrue(ingredient_exists('flour', PANCAKE_INGREDIENTS))
+        self.assertFalse(ingredient_exists('FLOUR', PANCAKE_INGREDIENTS))
+        self.assertTrue(ingredient_exists('salt', PANCAKE_INGREDIENTS))
+        self.assertFalse(ingredient_exists('sugar', PANCAKE_INGREDIENTS))
+
+    def test_fatten_pancakes(self):
+        fat_ingredients = fatten_pancakes(PANCAKE_INGREDIENTS)
+
+        for key in fat_ingredients.keys():
+            self.assertTrue(key in ['flour', 'eggs', 'milk', 'butter', 'salt'])
+
+        self.assertEqual(fat_ingredients['flour'], 2)
+        self.assertEqual(fat_ingredients['eggs'], 6)
+        self.assertEqual(fat_ingredients['milk'], 200)
+        self.assertTrue(fat_ingredients['butter'])
+        self.assertEqual(fat_ingredients['salt'], 0.001)
+
+        # test that PANCAKE_INGREDIENTS didn't change
+        self.test_pancake_ingredients()
+
+    def test_add_sugar(self):
+        with_sugar = add_sugar(PANCAKE_INGREDIENTS)
+
+        for key in with_sugar.keys():
+            self.assertTrue(key in ['flour', 'eggs', 'milk', 'butter', 'salt', 'sugar'])
+
+        self.assertEqual(with_sugar['flour'], 2)
+        self.assertEqual(with_sugar['eggs'], 4)
+        self.assertEqual(with_sugar['milk'], 200)
+        self.assertFalse(with_sugar['butter'])
+        self.assertEqual(with_sugar['salt'], 0.001)
+        self.assertTrue(with_sugar['sugar'])
+
+        # test that PANCAKE_INGREDIENTS didn't change
+        self.test_pancake_ingredients()
+
+    def test_remove_salt(self):
+        no_salt = remove_salt(PANCAKE_INGREDIENTS)
+
+        self.assertFalse('salt' in no_salt.keys())
+        for key in no_salt.keys():
+            self.assertTrue(key in ['flour', 'eggs', 'milk', 'butter'])
+
+        self.assertEqual(no_salt['flour'], 2)
+        self.assertEqual(no_salt['eggs'], 4)
+        self.assertEqual(no_salt['milk'], 200)
+        self.assertFalse(no_salt['butter'])
+
+        # test that PANCAKE_INGREDIENTS didn't change
+        self.test_pancake_ingredients()
+
+    def test_fibonacci_numbers(self):
+        self.assertEqual(FIBONACCI_NUMBERS, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144])
+
+
+    def test_add_fibonacci(self):
+        self.assertEqual(FIBONACCI_NUMBERS, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144])
+        self.assertEqual(add_fibonacci(FIBONACCI_NUMBERS)[-1], 233)
+        self.assertEqual(add_fibonacci(FIBONACCI_NUMBERS)[-1], 377)
+        self.assertEqual(add_fibonacci(FIBONACCI_NUMBERS)[-1], 610)
+
+    def test_fib_exists(self):
+        self.assertFalse(fib_exists(FIBONACCI_NUMBERS, 0))
+        self.assertTrue(fib_exists(FIBONACCI_NUMBERS, 1))
+        self.assertTrue(fib_exists(FIBONACCI_NUMBERS, 144))
+
+    def test_which_fib(self):
+        self.assertEqual(which_fib(FIBONACCI_NUMBERS, 1), 1)
+        self.assertEqual(which_fib(FIBONACCI_NUMBERS, 55), 10)
+
+        with self.assertRaises(ValueError):
+            which_fib(FIBONACCI_NUMBERS, 99999)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
