d6dd22093d591e7a85f1ed9563a1b120f60a517f
Alexander Todorov <atodorov@mrsenko.com>
2017-03-21 09:33:06 +0200

    Update assertFalse for prime number

--- module03/test_prime_number.py
+++ module03/test_prime_number.py
@@ -4,7 +4,7 @@ from solution import *
 class FirstDayTests(unittest.TestCase):
     def test_prime_number(self):
         self.assertTrue(prime_number(7))
-        self.assertFalse(prime_number(8))
+        self.assertEquals(False, prime_number(8))
 
 if __name__ == '__main__':
     unittest.main()
