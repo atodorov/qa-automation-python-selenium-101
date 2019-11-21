22f914ccff4cef9576532b8f754427361e83bd6a
Svetlozar Stoyanov <svetlozar.stoyanov@gmail.com>
2019-11-21 22:48:18 +0200

    Updated syntax of test_example.py to comply latest Python.
    Because PyCharm reports it as problem - underline file and module in red.

--- module05/test_example.py
+++ module05/test_example.py
@@ -4,30 +4,28 @@ from datetime import datetime
 class ExampleTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        print "setUpClass executes only once. CREATE DATABASE;"
-        print
+        print("setUpClass executes only once. CREATE DATABASE;")
 
     def setUp(self):
-        print "    setUp executes before every test method!"
+        print("    setUp executes before every test method!")
 
     def tearDown(self):
-        print "    tearDown executes after every test method!"
+        print("    tearDown executes after every test method!")
 
     @classmethod
     def tearDownClass(cls):
-        print
-        print "tearDownClass executes only once. DROP DATABASE;"
+        print("tearDownClass executes only once. DROP DATABASE;")
 
     def test_first_method(self):
-        print "        This is the first test. It will PASS"
+        print("        This is the first test. It will PASS")
         assert True
 
     def test_second_method(self):
-        print "        This is the second test. It will FAIL"
+        print("        This is the second test. It will FAIL")
         assert False
 
     def test_zzzzz(self):
-        print "        This is the last test. It will PASS"
+        print("        This is the last test. It will PASS")
         assert True
 
 
